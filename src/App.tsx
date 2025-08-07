e01778e4f0068eb316aafe5fed7eb4a4e0e41bb7
MrPublish <alireza_tavasoli76@yahoo.com>
2025-08-07 22:46:22 +0330

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+      dignissimos vero suscipit quasi omnis repellendus quis voluptatum
+      praesentium facere officiis ab cupiditate veniam enim nam at alias,
+      consequatur doloremque voluptatem?
+    </div>
+  );
+};
+
+export default App;
